2079aff035cbc1bb334eb94c7aff784379a8ae15
giriharan <49433475+giriharan@users.noreply.github.com>
2019-04-11 10:42:50 +0530

    Add files via upload

---
+++ session.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+?>
+<html>
+<form action= "textbox.php" method="post">
+<input type="text" name="uname"/><br>
+<input type="submit" name="submit" value="submit"><br>
+</form>
+</html>
+
