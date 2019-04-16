a585f8722e5a54f9e1ae8a51c73880c1e31bebc6
paigevino <vinodhinip2909@gmail.com>
2019-04-16 11:22:37 +0530

    commit by vino

--- session.php
+++ session.php
@@ -3,6 +3,7 @@ session_start();
 ?>
 <html>
 <form action= "textbox.php" method="post">
+giri
 <input type="text" name="uname"/><br>
 <input type="submit" name="submit" value="submit"><br>
 <input type="submit" name="submit" value="ok"><br>
