8e159d15b512a2785ce7a452c89daef958e964ac
giriharan <49433475+giriharan@users.noreply.github.com>
2019-04-30 14:33:47 +0530

    Update session.php

--- session.php
+++ session.php
@@ -2,7 +2,7 @@
 session_start();
 ?>
 <html>
-  input  type="name" value="submit"
+  input  type="name" value="submit"....
 <form action= "textbox.php" method="post">
 
 <input type="text" name="uname"/><br>
