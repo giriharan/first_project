c17894cc2af417b47a9928c2d319b0bcdc6efba3
giriharan <49433475+giriharan@users.noreply.github.com>
2019-04-30 14:18:23 +0530

    Update session.php

--- session.php
+++ session.php
@@ -7,6 +7,7 @@ giri
 <input type="text" name="uname"/><br>
 <input type="submit" name="submit" value="submit"><br>
 <input type="submit" name="submit" value="ok"><br>
+  print "Giriharan";
 </form>
 </html>
 
